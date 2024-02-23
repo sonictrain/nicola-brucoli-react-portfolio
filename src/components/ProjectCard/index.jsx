ca0a2980684963dbb4f8d63e2704f615b45778f4
sonictrain <nicola.brucoli92@gmail.com>
2024-02-23 00:57:32 +0000

    added card design for Projects

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,19 +1,47 @@
-import React from "react";
+import { React } from "react";
+import { Card, Typography, AspectRatio, Chip, IconButton } from '@mui/joy';
+import { GitHub } from '@mui/icons-material';
 
-const ProjectCard = ( props ) => {
+const ProjectCard = ({ id, title, image, description, githubLink, techStack }) => {
+
     return (
-        <div>
-            <h2>{props.title}</h2>
-            <img src={props.image} />
-            <p>{props.description}</p>
-            <a href={props.githubLink}>Link to GitHub</a>
-            <a href={props.liveLink}>Link to Live App</a>
-            <ul>{props.techStack.map((t) => (
-                    <li>{t}</li>
-                ))}
-            </ul>
-        </div>
+        <Card
+            key={id}
+            sx={{ width: 320 }}
+            color="primary"
+            size="md"
+            variant="plain">
+            <div>
+                <div>
+                    <Typography level="title-lg">{title}</Typography>
+                    <IconButton
+                        aria-label="Github Link"
+                        variant="plain"
+                        color="neutral"
+                        size="sm"
+                        onClick={() => window.open(`${githubLink}`, '_blank')}
+                        sx={{ position: 'absolute', top: '0.875rem', right: '0.5rem' }}>
+                        <GitHub />
+                    </IconButton>
+                </div>
+                <AspectRatio
+                    minHeight="120px"
+                    maxHeight="200px">
+                    <img
+                        src={image}
+                        loading="lazy"
+                        alt={title}
+                    />
+                </AspectRatio>
+                <Typography level="body-xs">{description}</Typography>
+                <div>
+                    {techStack.map((t, i) => (
+                        <Chip key={i} size="sm" variant="soft">{t}</Chip>
+                    ))}
+                </div>
+            </div>
+        </Card>
     )
 }
 
-export default ProjectCard
+export default ProjectCard;
