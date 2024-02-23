da30e4a18766273b46a394b19a097b27bc59e415
sonictrain <nicola.brucoli92@gmail.com>
2024-02-23 00:02:08 +0000

    working on the Contact Form

--- src/pages/ContactForm/index.jsx
+++ src/pages/ContactForm/index.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { FormControl, FormLabel } from '@mui/material';
+import { Box, Button, Input, Textarea } from '@mui/joy';
 
 const Contacts = () => {
   return (
     <>
-      <h3>Wanna have a chat?</h3>
-      <FormControl>
-        <FormLabel>Textfield</FormLabel>
-        <TextField></TextField>
-        <Button>Submit</Button>
-      </FormControl>
+      <h3>Let's chat!</h3>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+        <Textarea
+          color="primary"
+          minRows={4}
+          size="md"
+          variant="outlined"
+        />
+        <Input size="md" placeholder="your@email.com" variant="outlined" color="primary" endDecorator=
+          {
+            <Button variant="soft" size="sm">
+              Send!
+            </Button>
+          }
+          sx={{ '--Input-radius': '25px', '--Input-decoratorChildHeight': '50px' }}
+        />
+      </Box>
     </>
   );
 }
