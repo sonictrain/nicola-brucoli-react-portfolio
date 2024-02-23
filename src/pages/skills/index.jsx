589e0c71c46cb172e8b95588659045ae89e4c5d2
sonictrain <nicola.brucoli92@gmail.com>
2024-02-23 01:36:59 +0000

    improved skills page layout using simple cards

--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -1,20 +1,25 @@
 import skillData from "../../data/skills.json"
 import Skill from "../../components/Skill"
+import { Stack, Card } from '@mui/joy';
+
 
 const Skills = () => {
-    return (
-      <>
-        <h3>Skills Page</h3>
-        {skillData.map((s) => (
+  return (
+    <Stack spacing={1} direction="row" flexWrap="wrap" useFlexGap>
+
+      {skillData.map((s) => (
+        <Card size="lg" variant="plain">
           <Skill
             id={s.id}
             key={s.id}
             skillName={s.skillName}
             techIcon={s.techIcon}
-            />
-        ))}
-      </>
-    );
-  }
-  
+          />
+        </Card>
+      ))}
+    </Stack>
+  )
+
+}
+
 export default Skills;
