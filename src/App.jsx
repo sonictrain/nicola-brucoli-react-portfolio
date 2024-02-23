7e06651b2d9fbb668251ee0f2e51d34e7574f0ce
sonictrain <nicola.brucoli92@gmail.com>
2024-02-23 00:01:00 +0000

    removing custom css as not needed for now. using Inter font instead of roboto one. Setting CssBaseline as per JoyUI theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
 import React from 'react';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
@@ -6,36 +8,35 @@ import BioCard from './components/BioCard'
 import ContactForm from './pages/ContactForm';
 import Projects from './pages/Projects';
 import Skills from './pages/Skills';
-import './App.css';
 import Box from '@mui/system/Box';
 import Grid from '@mui/system/Unstable_Grid';
+import '@fontsource/inter';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 function App() {
   return (
     <>
-      <Router>
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={2} disableEqualOverflow={true}>
-            <Grid xs={12} md={6}>
-              <BioCard />
-              <Navigation />
+      <CssVarsProvider>
+        <CssBaseline />
+        <Router>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2} disableEqualOverflow={true}>
+              <Grid xs={12} md={6}>
+                <BioCard />
+                <Navigation />
+              </Grid>
+              <Grid xs={12} md={6}>
+                <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='contactForm' element={<ContactForm />} />
+                  <Route path='projects' element={<Projects />} />
+                  <Route path='skills' element={<Skills />} />
+                </Routes>
+              </Grid>
             </Grid>
-            <Grid xs={12} md={6}>
-              <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='contactForm' element={<ContactForm />} />
-                <Route path='projects' element={<Projects />} />
-                <Route path='skills' element={<Skills />} />
-              </Routes>
-            </Grid>
-          </Grid>
-        </Box>
-      </Router>
+          </Box>
+        </Router>
+      </CssVarsProvider>
     </>
   )
 }
