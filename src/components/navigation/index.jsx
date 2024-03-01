8e30b7919185a076b67e66cf6587fd80a638ca2e
sonictrain <nicola.brucoli92@gmail.com>
2024-03-01 17:06:44 +0000

    styling navigator with hovering and active effects

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -3,17 +3,57 @@ import { NavLink } from 'react-router-dom';
 function Navigation() {
   return (
     <nav className='mt-16'>
-      <div>
-        <NavLink to="/" end>Home</NavLink>
+      <div className="hover:text-accent-200 hover:before:content-['>']">
+        <NavLink
+          to="/"
+          className={
+            ({ isActive }) =>
+              isActive
+                ? "text-accent-200 font-bold before:content-['>']"
+                : ""
+          }
+          end>
+          Home
+        </NavLink>
       </div>
-      <div>
-        <NavLink to="skills" end>Skills</NavLink>
+      <div className="hover:text-accent-200 hover:before:content-['>']">
+        <NavLink
+          to="skills"
+          className={
+            ({ isActive }) =>
+              isActive
+                ? "text-accent-200 font-bold before:content-['>']"
+                : ""
+          }
+          end>
+          Skills
+        </NavLink>
       </div>
-      <div>
-        <NavLink to="projects" end>Projects</NavLink>
+      <div className="hover:text-accent-200 hover:before:content-['>']">
+        <NavLink
+          to="projects"
+          className={
+            ({ isActive }) =>
+              isActive
+                ? "text-accent-200 font-bold before:content-['>']"
+                : ""
+          }
+          end>
+          Projects
+        </NavLink>
       </div>
-      <div>
-        <NavLink to="contactForm" end>Contacts</NavLink>
+      <div className="hover:text-accent-200 hover:before:content-['>']">
+        <NavLink
+          to="contactForm"
+          className={
+            ({ isActive }) =>
+              isActive
+                ? "text-accent-200 font-bold before:content-['>']"
+                : ""
+          }
+          end>
+          Contacts
+        </NavLink>
       </div>
     </nav>
   );
