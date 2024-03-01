242351207003815dae3b29e962c4be9c42ee9c0e
sonictrain <nicola.brucoli92@gmail.com>
2024-03-01 17:34:14 +0000

    improving Home paragraph formatting

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,12 @@ import Signature from '../../components/Signature'
 
 const Home = () => {
   return (
-    <div className='text-lg flex flex-col justify-between h-full'>
+    <div className='flex flex-col justify-between h-full'>
       <div>
-      <p className="mb-3">Back in 2018, I decided to challenge myself and try my hand at developing functional prototypes of my interfaces and finished into the rabbit hole of coding and web development.
+      <p className="mb-5">Back in 2018, I decided to challenge myself and try my hand at developing functional prototypes of my interfaces and finished into the rabbit hole of coding and web development.
       Fast-forward to today, I've had the privilege of building software interfaces for startups, a university research projects, and tech companies from the aviation industry.</p>
 
-      <p className="mb-3">My main focus these days is building products and leading the software development for our clients at Weave Intelligence.
+      <p className="mb-5">My main focus these days is building products and leading the software development for our clients at Weave Intelligence.
       I most enjoy working with new technology building software which is done right - exactly in the sweet spot where design and engineering meet - you know, things that look good but are also done properly under the hood.
       In my free time, I love</p>
 
