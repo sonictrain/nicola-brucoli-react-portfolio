396c74c2806ba5b9bd5e4bd1ffb6bcb3bebc9c54
sonictrain <nicola.brucoli92@gmail.com>
2024-03-03 20:08:50 +0000

    improving responsivness

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,34 @@ import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Bio from './components/Bio'
 import Footer from './components/Footer'
-import ContactForm from './pages/ContactForm';
+import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 import Skills from './pages/Skills';
 import "inter-ui/inter.css";
 
 function App() {
   return (
-    <div className='grid grid-cols-2 h-dvh bg-midnightBlue-200 text-stone-200'>
+    <div className='grid grid-cols-1 md:grid-cols-2 bg-midnightBlue-200 text-stone-200'>
       <Router>
-        <div className='overflow-y-auto flex flex-col h-screen justify-between p-20'>
+        <div className='overflow-y-auto flex flex-col md:h-screen justify-between p-10 md:p-14 lg:p-20'>
           <div>
             <Bio />
             <Navigation />
           </div>
+          <div className='hidden md:block'>
             <Footer />
+          </div>
         </div>
-        <div className='overflow-y-auto px-20 pt-20'>
+        <div className='px-10 pt-10 md:px-14 md:pt-14 lg:p-20'>
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='contactForm' element={<ContactForm />} />
+            <Route path='contact' element={<Contact />} />
             <Route path='projects' element={<Projects />} />
             <Route path='skills' element={<Skills />} />
           </Routes>
+        </div>
+        <div className='px-10 pb-10 mt-auto md:px-14 md:pb-14 lg:px-20 lg:pb-20 md:hidden'>
+            <Footer />
         </div>
       </Router>
     </div>
--- src/components/Signature/index.jsx
+++ src/components/Signature/index.jsx
@@ -1,6 +1,14 @@
 const Signature = () => {
     return (
-        <p className='text-md py-20 text-stone-900'>Designed in <a href={'https://figma.com/'} className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200">Figma</a> and coded in <a href={'https://code.visualstudio.com/'} className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200">Visual Studio Code</a> by me. Built with <a href={'https://react.dev/'} className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200">React</a>, <a href={'https://mui.com/'} className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200">Material UI</a> and <a href={'https://tailwindcss.com/'} className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200">Tailwind CSS</a> under the hood, deployed with <a href={'https://netlify.com/'} className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200">Netlify</a>. All text is set in the <span href={'https://rsms.me/inter/'} className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200">Inter</span> typeface.</p>
+        <p className='text-md pt-10 md:pt-14 lg:pt-20 text-stone-900'>Designed in 
+            <a href={'https://figma.com/'}              className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200"> Figma</a> and coded in 
+            <a href={'https://code.visualstudio.com/'}  className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200"> Visual Studio Code</a> by me. Built with 
+            <a href={'https://react.dev/'}              className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200"> React</a>, 
+            <a href={'https://mui.com/'}                className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200"> Material UI</a> and 
+            <a href={'https://tailwindcss.com/'}        className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200"> Tailwind CSS</a> under the hood, deployed with 
+            <a href={'https://netlify.com/'}            className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200"> Netlify</a>. All text is set in the 
+            <a href={'https://rsms.me/inter/'}          className="text-stone-700 hover:text-accent-200 hover:shadow-accent-200"> Inter</a> typeface.
+        </p>
     )
 }
 
