bf4d0b07ba1cf7beb226af521fd2443fe0c3ca37
sonictrain <nicola.brucoli92@gmail.com>
2024-03-13 19:16:08 +0000

    axios get query refactor

--- src/components/ActivityChart/index.jsx
+++ src/components/ActivityChart/index.jsx
@@ -1,4 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import Chart from "react-apexcharts";
+import axios from 'axios';
 
 const chartConfig = {
     type: "line",
@@ -92,6 +94,35 @@ const chartConfig = {
 };
 
 const ActivityChart = () => {
+
+    const [activityData, setActivityData] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            const axiosConfig = {
+                method: 'GET',
+                url: `https://wakatime.com/share/@018dc7ca-a471-4528-9e24-51a965772997/f2b55831-8585-40dd-ab6f-eefc918e7e08.json`,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+            try
+            {
+                const response = await axios.request(axiosConfig);
+                console.log(response.data);
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchData();
+
+    }, []);
+
     return (
         <Chart {...chartConfig} />
     );
