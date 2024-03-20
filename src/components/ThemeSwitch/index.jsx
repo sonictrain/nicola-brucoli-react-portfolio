e3042fd18f71bff21942fe0bf1137b2738ada8ec
sonictrain <nicola.brucoli92@gmail.com>
2024-03-20 22:19:13 +0000

    adding a theme switcher in preparation of dark/light theme

---
+++ src/components/ThemeSwitch/index.jsx
@@ -0,0 +1,11 @@
+import { Switch } from "@material-tailwind/react";
+ 
+const ThemeSwitch = () => {
+  return (
+    <div className="fixed top-0 right-0">
+      <Switch />
+    </div>
+  );
+}
+
+export default ThemeSwitch;
