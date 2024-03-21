9c79c114e35d0c63887c81ffca0570f3da5b064f
sonictrain <nicola.brucoli92@gmail.com>
2024-03-21 00:05:12 +0000

    Added color and padding styling

--- src/components/ThemeSwitch/index.jsx
+++ src/components/ThemeSwitch/index.jsx
@@ -2,8 +2,13 @@ import { Switch } from "@material-tailwind/react";
  
 const ThemeSwitch = () => {
   return (
-    <div className="fixed top-0 right-0">
-      <Switch />
+    <div className="fixed top-0 right-0 p-5">
+      <Switch 
+        className="h-full w-full checked:bg-accent-900"
+        circleProps={{
+          className: "before:hidden border-none bg-accent-200",
+        }}
+      />
     </div>
   );
 }
